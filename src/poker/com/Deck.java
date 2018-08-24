596b85a735b3fdc1f0151d531553104c51f65f6f
LeeJaeEe <38426064+JaeEeLee@users.noreply.github.com>
2018-08-24 17:09:44 +0900

    Add files via upload

--- src/poker/com/Deck.java
+++ src/poker/com/Deck.java
@@ -1,9 +1,37 @@
 package poker.com;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
-//맞춤법 요정이 왔다리오졌따리세종대왕느님뿌꾸빵뿌꾸빵
-//맞춤법 요정이 왔다리오졌따리세종대왕느님뿌꾸빵뿌꾸빵
-	///ㅇ란어리ㅏ
-	//sdfsfd
-	//sdfsfd
+
+	List<Card> Deck = new ArrayList<Card>();
+	
+	private final String[] cardShape = { "♣", "♥", "♦", "♠" };
+	private final int[] cardShapePoint = { 1, 2, 3, 4 };
+	private final String[] cardNum = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+	private final int[] cardNumPoint = { 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+	public void deckInit() {
+		for (int i = 0; i < cardShape.length; i++) {
+			for (int j = 0; j < cardNum.length; j++) {
+				Card card = new Card(cardShape[i], cardShapePoint[i], cardNum[j], cardNumPoint[j]);
+				Deck.add(card);
+			}
+		}
+	}
+	
+	public void deckShuffle() {
+		Collections.shuffle(Deck);
+	}	
+	
+	public Card getCard() {
+		Card card;
+		card = Deck.get(0);
+		Deck.remove(0);
+		
+		return card;
+	}
+	
 }
--- src/poker/com/Main.java
+++ src/poker/com/Main.java
@@ -1,11 +1,28 @@
 package poker.com;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Deck deck = new Deck();
+		
+		deck.Deck.size();
+		deck.getCard();
+		deck.deckShuffle();
+		
+		deck.deckInit();
+//		deck.deckShuffle();
+		
+//		for (int i = 0; i < deck.Deck.size(); i++) {
+//			System.out.println(deck.Deck.get(i));
+//		}
+//		System.out.println();
+		
+		Card c = deck.getCard();
+		System.out.println(c);
 		
 		
 	}
-
 }
